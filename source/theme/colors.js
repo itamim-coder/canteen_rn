7e61016ba602b4ece14b77d0ebfd9e89efd9fe48
itamim-coder <itamim12202@gmail.com>
2022-07-22 00:33:03 +0600

    structure init

---
+++ source/theme/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  black: '#070724',
+  white: '#fff',
+  darkGrey: '#38384f',
+  grey: '#838391',
+  lightGreen: '#419ebb',
+  orange: '#EDA249',
+  purple: '#6f2ed6',
+  darkOrange: '#D14c32',
+  red: '#D83A34',
+};
---
+++ source/theme/typography.js
@@ -0,0 +1,5 @@
+export const typography = {
+  primary: 'Roboto-Regular',
+  primaryBold: 'Roboto-Regular',
+  secondary: 'Roboto-Regular',
+};
